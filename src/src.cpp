b1ed9181edcd95be69f3d1c479f362ec1aa877a7
SpaceFishGameDev <ultragamesalot@gmail.com>
2022-03-13 00:02:53 +0200

    Read file added

--- src/src.cpp
+++ src/src.cpp
@@ -8,7 +8,42 @@ const char* sift(int* argc, char*** argv)
 	return r;
 }
 
-int main()
-{
 
+std::string ReadAsciiFile(std::string path)
+{
+	std::string r = "";
+	std::vector<char> v;
+	if (FILE *fp = fopen(path.c_str(), "r"))
+	{
+		char buf[1024];
+		while (size_t len = fread(buf, 1, sizeof(buf), fp))
+			v.insert(v.end(), buf, buf + len);
+		fclose(fp);
+	}
+	for(char c : v)
+	{
+		r += c;
+	}
+	return r;
 }
+
+int main(int argc, char** argv)
+{
+	const char* program = sift(&argc, &argv);
+	if(argc == 0)
+	{
+		printf("ERROR: File must be inputted");
+		return 0;
+	}
+	std::string cmd = sift(&argc, &argv);
+	if(cmd == "-h")
+	{
+
+	}
+	else
+	{
+		std::string fpath = cmd;
+
+	}
+}
+
