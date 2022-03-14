a9c59a3bb96d1dfbe3f46c818baca3bdee0a7c28
Adam Faur <adam.faur@gmail.com>
2022-03-13 18:36:27 -0700

    Add using std to lexer

--- src/Lexer.h
+++ src/Lexer.h
@@ -1,24 +1,32 @@
-
 #include<iostream>
 #include<vector>
 
-std::vector<std::string> getRows(std::string src)
+using namespace std;
+
+vector<string> getRows(string src)
 {
 	const char* src_chr = src.c_str();
-	std::string temp = "";
-	std::vector<std::string> rows;
+
+	string temp = "";
+	vector<string> rows;
+
 	while(*src_chr)
 	{
 		char c = *src_chr;
+
 		if(c == '\n')
 		{
 			rows.push_back(temp);
-		}else
+		}
+		else
 		{
 			temp += c;
 		}
+
 		++src_chr;
 	}
+
 	rows.push_back(temp);
+
 	return rows;
 }
